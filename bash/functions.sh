2cfefdff782e9a270dcecc9338556bab08500407
Julio Angulo <solojoas@gmail.com>
2021-04-14 14:10:13 -0500

    Added variable  assignment grep

--- bash/functions.sh
+++ bash/functions.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
+# grep function declaration
 function grepFunc()
 {
     grep -rnw -E "\w+ $1 *"
+}
+
+# grep variable assignment
+function grepVarA()
+{
+    grep -rnE "$1\s*=" * 
 }
