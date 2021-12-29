e7b44811d194433c33549aa3964b4b3d484e4f5c
Julio Angulo <solojoas@gmail.com>
2021-12-29 14:30:19 -0500

    Added autoinstall script

---
+++ tools/install.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+DEBUG_ON=1
+if $DEBUG_ON; then
+    set -x
+fi
+
+echo installing dot files settings...
+
+DOT_FILES_PATH="$HOME/joasDotSettings"
+
+# mkdir ~/joasDotSettings
+cd ~/joasDotSettings
+# 
+# git clone https://github.com/joas77/dotFiles.git
+
+cd dotFiles
+
+cp vim/vimrc ~/.vimrc
+cp vim/gvimrc ~/.gvimrc
+
+RCFILE="$HOME/.bashrc"
+
+TODAY=$(date -I)
+BACKUP_RC="${RCFILE}_backup_${TODAY}"
+echo "backing up current $RCFILE to ${BACKUP_RC} ..."
+
+cp "$RCFILE" "$BACKUP_RC"
+
+cat bash/bash_profile >> $RCFILE
+echo "source \"${DOT_FILES_PATH}/dotFiles/bash/functions.sh\"" >> $RCFILE ;
+
